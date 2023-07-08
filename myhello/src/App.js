76aab8896f0abba3d98556e834cea1179161ce39
maromo71 <professormoraes@gmail.com>
2023-07-08 12:15:48 -0300

    Material 01

---
+++ myhello/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Meu App React</h1> 
+      <h2>Hello World!</h2>
+    </div>
+  );
+}
+
+export default App;
