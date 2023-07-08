702301b4c2622eaaf6cfa3c73b7f6b0a10941cf2
maromo71 <professormoraes@gmail.com>
2023-07-08 12:47:05 -0300

    Material 02

---
+++ sample02/src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css';
+
+function Card(){
+    return(
+        <div className="card-container">
+            <p>Componentes</p>
+            <p>Facilita manter partes menores funcionando</p>
+            <p>Você pode reutilizá-los, ou seja, reaproveitá-los</p>
+        </div>
+    );
+}
+
+export default Card;
---
+++ sample02/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css';
+
+function Footer() {
+    return (
+        <footer>Desenvolvimento com React</footer>
+    );
+}
+
+export default Footer;
